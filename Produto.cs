9f0d47a478e9b69410bb5e7603f51825599d6983
Beatriz-Claus <61596748+Beatriz-Claus@users.noreply.github.com>
2020-07-03 08:56:01 -0300

    Aula 27 - Finalizado

---
+++ Produto.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace ManipulandoCsv
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        private const string PATH = "Database/Produto.csv";
+
+        public Produto(){
+            // string pasta = PATH.Split('/')[0];
+            // if(!Directory.Exists(pasta)){
+            //     Directory.CreateDirectory(pasta);
+            // }
+
+
+            if(!File.Exists(PATH)){
+                File.Create(PATH).Close();
+            }
+        }
+        public void Cadastrar(Produto prod){
+            string[] linha = new string[] { PrepararLinha(prod) };
+            File.AppendAllLines(PATH, linha);
+        }
+        private string PrepararLinha(Produto p){
+            return $"codigo={p.Codigo};nome={p.Nome};preço={p.Preco}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿  
+using System;
+using System.Collections.Generic;
+
+namespace  ManipulandoCsv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p1 = new Produto();
+            p1.Codigo = 1;
+            p1.Nome = "Samsung Galaxy A20";
+            p1.Preco = 3900.99f;
+
+            p1.Cadastrar(p1);
+        }
+    }
+}
